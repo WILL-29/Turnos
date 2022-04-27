f09fdf9992514d6798fb61288eb37d5c2e0f7482
WILL-29 <willdelarosa29@hotmail.com>
2022-04-27 00:39:07 -0400

    Metodo Edit

--- Controllers/EspecialidadController.cs
+++ Controllers/EspecialidadController.cs
@@ -14,5 +14,35 @@ namespace Turnos.Controllers
         {
             return View(_contexto.Especialidades.ToList());
         }
+        public IActionResult Edit(int? id)
+        //En este caso usamos el ? para convertir int en nulleable
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var especialidad = _contexto.Especialidades.Find(id);
+            
+            if (especialidad == null) //Esta validación es por si nos pasan el parámetro, pero este ID no existe en la base de datos
+            {
+                return NotFound();
+            }
+            return View(especialidad);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, [Bind("Id_Especialidad,Descripcion")] Especialidad especialidad)
+        {
+            if (id != especialidad.ID_Especialidad)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid == true)
+            {
+                _contexto.Update(especialidad);
+                _contexto.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(especialidad);
+        }
     }
 }
