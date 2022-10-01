c73880e08d23a7d2070a3f8ee676e12add0b2983
WILL-29 <willdelarosa29@hotmail.com>
2022-09-30 20:17:52 -0400

    Delete Method
    Se creó el controlador "Eliminar" y sus action.
    Se creó la vista Eliminar y sus botones.

--- Controllers/EspecialidadController.cs
+++ Controllers/EspecialidadController.cs
@@ -10,12 +10,21 @@ namespace Turnos.Controllers
         {
             _contexto = Contexto;
         }
+
+        // Esto es un Action (método que devuelve un IActionResult), los métodos siempre devuelven una vista con su mismo nombre (también puedes enviar información a la Vista),
+        //por ejemplo este devuelve la vista Index (Las vistas de este controlador están en la carpeta con su nombre, dentro de la carpeta View. (View\Especialidad))
         public IActionResult Index()
         {
+
             return View(_contexto.Especialidades.ToList());
+
+            //La prueba de abajo, solo la hice para probar y funcionó como esperaba
+            //return View(_contexto.Especialidades.ToList().Where(w => w.Descripcion == "Pediatria"));
         }
-        public IActionResult Edit(int? id)
+       
+
         //En este caso usamos el ? para convertir int en nulleable
+        public IActionResult Edit(int? id)
         {
             if (id == null)
             {
@@ -29,6 +38,8 @@ namespace Turnos.Controllers
             }
             return View(especialidad);
         }
+
+        //Con Http Post indicamos que este método, envía info
         [HttpPost]
         public IActionResult Edit(int id, [Bind("ID_Especialidad,Descripcion")] Especialidad especialidad)
         {
@@ -43,6 +54,34 @@ namespace Turnos.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(especialidad);
+        }
+
+
+        //Este es tipo GET, este método solo obtiene información de lo que se va a eliminar y confirma su existencia, este no realiza ningún cambio
+        public IActionResult Delete(int? id)
+        {
+                if (id == null)
+                {               
+                    return NotFound();
+                }
+            var especialidad = _contexto.Especialidades.FirstOrDefault(w => w.ID_Especialidad == id);
+
+            if (especialidad == null)
+            {
+                return NotFound();
+            }
+
+            return View(especialidad); 
+        }
+
+        //Este Mëtodo es tipo POST, este es que hace el cambio
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var especialidad = _contexto.Especialidades.FirstOrDefault(w => w.ID_Especialidad == id);
+            _contexto.Especialidades.Remove(especialidad);
+            _contexto.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
