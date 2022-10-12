1e1f33f73148306de86ebd5e13ba3e8eac64a6d9
WILL-29 <willdelarosa29@hotmail.com>
2022-10-12 08:37:20 -0400

    Metodos Agregados
    Se agregaron el Mëtodo Details y los 2 métodos de crear para el modelo Paciente

--- Controllers/PacienteController.cs
+++ Controllers/PacienteController.cs
@@ -18,5 +18,37 @@ namespace Turnos.Controllers
         {
             return View(await _contexto.Pacientes.ToListAsync());
         }
+
+        public async Task<IActionResult> Details(int? Id)
+        {
+            if (Id== null)
+            {
+                return NotFound();
+            }
+            var paciente = await _contexto.Pacientes.FirstOrDefaultAsync(p => p.ID_Paciente == Id);
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+
+            return View(paciente);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID_Paciente,Nombre,Apellido,Direccion,Telefono,Email")]Paciente paciente)
+        {
+            if (ModelState.IsValid)
+            {
+            _contexto.Pacientes.Add(paciente);
+            await _contexto.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
     }
 }
