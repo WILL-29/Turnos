600ed98c7c6e44eccc28f54127f0d714e2819eac
WILL-29 <willdelarosa29@hotmail.com>
2022-10-12 22:57:28 -0400

    Vista Index y metodos restantes
    Se creó la vista Index, Metodos Edit, Metodos y metodos Delete.

--- Controllers/PacienteController.cs
+++ Controllers/PacienteController.cs
@@ -33,6 +33,8 @@ namespace Turnos.Controllers
 
             return View(paciente);
         }
+
+
         [HttpGet]
         public IActionResult Create()
         {
@@ -49,6 +51,64 @@ namespace Turnos.Controllers
             return RedirectToAction(nameof(Index));
             }
             return View();
+        }
+        
+        public async Task<IActionResult> Edit(int? Id)
+        {
+            if (Id==null)
+            {
+             return NotFound();
+            }
+            var paciente = await _contexto.Pacientes.FirstOrDefaultAsync(p => p.ID_Paciente == Id);
+            if (paciente == null)
+            {
+                return View(paciente);
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Edit(int Id, [Bind("ID_Paciente,Nombre,Apellido,Direccion,Telefono,Email")]Paciente paciente)
+        {
+            if(paciente.ID_Paciente != Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _contexto.Pacientes.Update(paciente);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(paciente);
+        }
+
+
+        public async Task<IActionResult> Delete(int? Id)
+        {
+            if (Id==null)
+            {
+             return NotFound();
+            }
+            var paciente = await _contexto.Pacientes.FirstOrDefaultAsync(p => p.ID_Paciente == Id);
+            if (paciente == null)
+            {
+                return View(paciente);
+            }
+            return View();
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+                var paciente = await _contexto.Pacientes.FirstOrDefaultAsync(p => p.ID_Paciente == Id);
+                _contexto.Pacientes.Remove(paciente);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));    
         }
     }
 }
